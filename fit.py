210f3ab6ae58c3699068b3dcd7d34e9de98ae504
CBaquero <cbm@di.uminho.pt>
2026-03-22 13:08:06 +0000

    Experiment 74: Shifted power law log10(d+100)

--- fit.py
+++ fit.py
@@ -30,7 +30,7 @@ from scipy.optimize import curve_fit, minimize
 
 def formula(days, a, b):
     """Shifted power law: log10(d + 200) models earlier effective genesis."""
-    return a * np.log10(days + 200.0) + b
+    return a * np.log10(days + 100.0) + b
 
 
 # Initial parameter guesses for curve_fit
