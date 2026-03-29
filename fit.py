b03b41f4f8fc45d07209b83781c812466736ade5
nkuhuxu <2030443724@qq.com>
2026-03-29 22:24:02 +0800

    EWMA span=0.5

--- fit.py
+++ fit.py
@@ -77,7 +77,7 @@ def model_fn(train_days, train_log_prices, test_days):
     residuals = recent_prices - formula(recent_days, a, b, c, d)
 
     # EWMA: exponential weights, more weight to recent
-    span = 1.0
+    span = 0.5
     alpha = 1.0 - np.exp(-1.0 / span)
     n = len(residuals)
     weights = np.array([(1 - alpha) ** (n - 1 - i) for i in range(n)])
