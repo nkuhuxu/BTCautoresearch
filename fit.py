9ba0f8b4d7deb8d86b85aba40cfc4014fff2f553
nkuhuxu <2030443724@qq.com>
2026-03-29 21:01:20 +0800

    ensemble_[107,127,147]

--- fit.py
+++ fit.py
@@ -109,7 +109,7 @@ def model_fn(train_days, train_log_prices, test_days):
 
     # Correction relative to past trend (same convention)
     corrections = []
-    for hl in [97.0, 127.0, 157.0]:
+    for hl in [107.0, 127.0, 147.0]:
         decay = np.exp(-np.log(2) * dt / hl)
         corrections.append((r0 + slope * dt) * decay)
     ensemble_correction = np.mean(corrections, axis=0)
