d28a56b3e5de821264cdbf11e6455432ae5df825
nkuhuxu <2030443724@qq.com>
2026-03-29 21:05:59 +0800

    half-life=117

--- fit.py
+++ fit.py
@@ -109,7 +109,7 @@ def model_fn(train_days, train_log_prices, test_days):
 
     # Correction relative to past trend (same convention)
     corrections = []
-    for hl in [127.0]:
+    for hl in [117.0]:
         decay = np.exp(-np.log(2) * dt / hl)
         corrections.append((r0 + slope * dt) * decay)
     ensemble_correction = np.mean(corrections, axis=0)
