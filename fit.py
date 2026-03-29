da9f68ce72b2447d66a0841ef3136142ac9045bb
nkuhuxu <2030443724@qq.com>
2026-03-29 23:23:49 +0800

    trend damping factor=0.02

--- fit.py
+++ fit.py
@@ -95,7 +95,7 @@ def model_fn(train_days, train_log_prices, test_days):
         trend = 0.0
 
     # Blend: deviation decays, trend continues with small damping
-    trend_damping = np.exp(-0.05 * dt / half_life)  # Slight damping for trend
+    trend_damping = np.exp(-0.02 * dt / half_life)  # Slight damping for trend
     return formula(test_days, a, b, c, d) + deviation * decay + trend * trend_damping
 
 
