1343251356507d81e385d327a80bd3a6dca3f480
nkuhuxu <2030443724@qq.com>
2026-03-29 22:53:26 +0800

    P0=[-1.0, 7.0, -5.0, 500.0]

--- fit.py
+++ fit.py
@@ -37,7 +37,7 @@ def formula(days, a, b, c, d):
 
 
 # Initial parameter guesses for curve_fit
-P0 = [-0.5, 6.0, -10.0, 400.0]
+P0 = [-1.0, 7.0, -5.0, 500.0]
 
 # Parameter bounds (use None for unbounded)
 BOUNDS = ([-np.inf, -np.inf, -np.inf, 1.0], [np.inf, np.inf, np.inf, 2000.0])
