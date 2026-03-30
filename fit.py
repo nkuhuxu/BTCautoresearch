6af7ee8f146506b01211b70149b4ae4256969ab0
nkuhuxu <2030443724@qq.com>
2026-03-30 13:15:44 +0800

    gamma=1.35

--- fit.py
+++ fit.py
@@ -91,7 +91,7 @@ def model_fn(train_days, train_log_prices, test_days):
     n_trend = min(30, len(residuals))
     if n_trend > 1:
         x = np.arange(n_trend)
-        w = np.exp(1.30 * x / n_trend)  # More weight on recent points
+        w = np.exp(1.35 * x / n_trend)  # More weight on recent points
         slope, _ = np.polyfit(x, residuals[-n_trend:], 1, w=w)
         trend = slope * dt / 3.4  # Scale by days
     else:
